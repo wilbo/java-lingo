67a07e9a98f76a4cfd8524b44d5e1c4682651b8d
Wilbert Schepenaar <wilbertschepenaar@hotmail.com>
2015-08-17 13:46:35 +0200

    allow a 5 letter word argument

--- Lingo.java
+++ Lingo.java
@@ -2,7 +2,12 @@ public class Lingo {
 
 	public static void main(String[] args) {
 
-		Game game = new Game("lingo");
+		if (args.length == 0 || args[0].length() != 5) {
+			System.out.println("Please enter a 5 letter word word.");
+			System.exit(0);
+		}
+
+		Game game = new Game(args[0]);
 		Prompter prompter = new Prompter(game);
 
 		System.out.printf("Let's play Lingo. The answer is a 5 letter word.\n");
